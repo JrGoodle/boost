d23366c54d2761257d259d85eb540434d9c49e2d
Joe DeCapo <joe@polka.cat>
2024-02-03 12:14:45 -0600

    Add script to build boost-iosx

---
+++ script/build-boost.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+
+set -euo pipefail
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+
+pushd 'boost-iosx'
+
+bundle exec scripts/build.sh
