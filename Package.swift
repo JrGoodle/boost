16cfcb1224b6ff7ce786b67f8ae5548d1d60f5a1
Joe DeCapo <joe@polka.cat>
2024-02-03 13:21:02 -0600

    Add  1.84.0 checksum

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let version = "1.84.2"
+let version = "1.84.0"
 let moduleName = "boost"
-let checksum = ""
+let checksum = "674de701533107501b8cb0d980328804d2cfb9d301e0da4bc9522d8752674030"
 
 let package = Package(
     name: moduleName,
