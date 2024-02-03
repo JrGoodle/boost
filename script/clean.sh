c419d89de8b40e501a41f05a2493301ff0adb2f2
Joe DeCapo <joe@polka.cat>
2024-02-03 12:34:30 -0600

    Add clean script

---
+++ script/clean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+
+rm -rf \
+    boost-iosx/boost \
+    boost-iosx/boost*.tar.bz2 \
+    boost-iosx/frameworks* \
+    boost-iosx/scripts/Pods \
+    boost-iosx/scripts/Podfile.lock \
+    boost-iosx/scripts/ICUdep.xcworkspace
